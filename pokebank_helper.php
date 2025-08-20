a3c6b7fb17e518c21996bc1829f215647579d8e9
Ferrisx4 <ferrisx4@gmail.com>
2025-08-19 20:35:05 -0400

    Print misspelling hints at end of output

--- pokebank_helper.php
+++ pokebank_helper.php
@@ -80,6 +80,7 @@ function print_placement($dex_no) {
 $inputs = $argv;
 // Get rid of the program name lol.
 array_shift($inputs);
+$misspell = 0;
 foreach ($inputs as $input) {
   // If a Dex no was given.
   if (is_numeric($input)) {
@@ -89,18 +90,25 @@ foreach ($inputs as $input) {
   else {
     $dex_no = get_pokemon_number($input);
     if (!$dex_no) {
-      echo $input . "? That Pokémon's name could not be found. Did you misspell it?\n";
-      echo "Check the following:\n";
-      echo " - Names are case-sensitive\n";
-      echo " - Names with spaces and apostrophes need to be escaped, as such:\n";
-      echo "   - Mr. Mime -> Mr.\ Mime\n";
-      echo "   - Farfetch'd -> Farfetch\'d\n";
-      echo " - For Nidoran♀ and Nidoran♂, you can either use these characters or F and M, respectively:\n";
-      echo "   - Nidran♀ -> Nidoran♀ OR NidoranF\n";
+      $misspell = 1;
+      echo $input . "? That Pokémon's name could not be found. See the note below about spelling common spelling problems.\n";
+
     }
     else {
       print_placement($dex_no);
     }
   }
+}
+
+// Print helpful message AFTER all the other Pokémon have been printed.
+if ($misspell) {
+  echo "Check the following:\n";
+  echo " - Names are case-sensitive\n";
+  echo " - Names with spaces and apostrophes need to be escaped, as such:\n";
+  echo "   - Mr. Mime -> Mr.\ Mime\n";
+  echo "   - Farfetch'd -> Farfetch\'d\n";
+  echo " - For Nidoran♀ and Nidoran♂, you can either use these characters or F and M, respectively:\n";
+  echo "   - Nidran♀ -> Nidoran♀ OR NidoranF\n";
+  
 }
 
